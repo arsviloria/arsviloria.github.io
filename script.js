67be9e4677d3a271ee59298b2de7afd6684e3d09
Amiel Radnitz Viloria <arsviloria@pcu.edu.ph>
2026-08-27 18:46:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.slideshow-container').forEach((container) => {
     const nextBtn = container.querySelector('.next');
  
     let current = 0;
-    let timer = null;
+
  
     function showSlide(index) {
         if (index >= slides.length) index = 0;
@@ -19,33 +19,25 @@ document.querySelectorAll('.slideshow-container').forEach((container) => {
         if (dots[current]) dots[current].classList.add('active');
     }
  
-    function startAutoplay() {
-        timer = setInterval(() => showSlide(current + 1), 4000);
-    }
- 
-    function resetAutoplay() {
-        clearInterval(timer);
-        startAutoplay();
-    }
+   
  
     prevBtn.addEventListener('click', () => {
         showSlide(current - 1);
-        resetAutoplay();
+        
     });
  
     nextBtn.addEventListener('click', () => {
         showSlide(current + 1);
-        resetAutoplay();
+        
     });
  
     dots.forEach((dot, i) => {
         dot.addEventListener('click', () => {
             showSlide(i);
-            resetAutoplay();
+            
         });
     });
  
     showSlide(0);
-    startAutoplay();
 });
  
